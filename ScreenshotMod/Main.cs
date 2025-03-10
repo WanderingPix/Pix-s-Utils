3401786c7157abbd8938a4d1a306dd8f43b0c7de
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-03-10 21:31:28 +0100

    Create Main.cs

---
+++ ScreenshotMod/Main.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace ScreenshotMod
+{
+  public class Main
+  {
+    public void FixedUpdate()
+    {
+      if (Input.GetKeyDown(KeyCode.F2))
+      {
+        //Insert Screenshot Logic here
+      }
+    }
+  }
+}
