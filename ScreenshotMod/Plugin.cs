bcc2d7099fe4332456f817a0ee34ea70ed11db0c
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-03-10 22:59:49 +0100

    Create Plugin.cs

---
+++ ScreenshotMod/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Reactor;
+using Reactor.Utilities;
+
+namespace ScreenshotMod;
+
+[BepInAutoPlugin]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+public partial class TemplatePlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+    public override void Load()
+    {
+    Harmomy.PatchAll();
+    }
+}
+    
