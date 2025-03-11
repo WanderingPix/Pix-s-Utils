57dec21cf85d89d8b062e8499cc303571e92fa61
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-03-11 08:33:38 +0100

    Add files via upload

---
+++ ColorfulBubbles/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Reactor;
+using Reactor.Networking;
+using Reactor.Networking.Attributes;
+using Reactor;
+using Reactor.Utilities;
+
+namespace ColorfulBubbles;
+
+[BepInAutoPlugin("ColorfulBubbles", "ColorfulBubbles")]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+[BepInDependency("com.slushiegoose.townofus", BepInDependency.DependencyFlags.SoftDependency)]
+public partial class CBPlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+    public ConfigFile GetConfigFile() => Config;
+    public override void Load()
+    {
+        Harmony.PatchAll();
+    }
+    
+}
---
+++ ColorfulBubbles/patch.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using HarmonyLib;
+using BepInEx;
+using ColorfulBubbles;
+
+namespace ColorfulBubbles
+{
+    [HarmonyPatch(typeof(ChatBubble), nameof(ChatBubble.SetText))]
+    class ColorBubble
+    {
+        public static void Postfix(ChatBubble __instance)
+        {
+            foreach (var player in PlayerControl.AllPlayerControls)
+            {
+                if (player.Data == __instance.playerInfo && !player.Data.IsDead)
+                {
+                    __instance.Background.color = player.Data.Color;
+                    if (player.Data.Color == new Color(0f, 0f, 0f, 1f))
+                    {
+                        __instance.Background.color = new Color(1f, 1f, 1f, 1f);
+                    }
+                }
+            }
+        }
+    }
+}
